21272f4e71b8850aadfd08cd0fe8e3b71e014cbb
RhythmDeolus <122043902+RhythmDeolus@users.noreply.github.com>
2024-03-01 20:53:03 +0530

    updated cdd alias to work better

--- shell-scripts/alias.sh
+++ shell-scripts/alias.sh
@@ -1,6 +1,6 @@
 export HOME2="/mnt/c/Users/rhyth"
 alias cdd="cd \$(z | fzf | awk '{print \$2}' | grep '^' || find ~ -type d | fzf | grep '^' || pwd)"
-alias his="\$(history | fzf | awk  '{sub(/^\\s[0-9]+\\s/, \"\", \$0); print \$0}')"
+alias his="\$(history | fzf --tiebreak=index --tac | awk  '{sub(/^\\s[0-9]+\\s/, \"\", \$0); print \$0}')"
 function vact() {
 source $1"/bin/activate"
 }
