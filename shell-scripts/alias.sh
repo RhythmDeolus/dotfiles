d2895db327d1ba08256a6c167123457844dd4a8a
RhythmDeolus <122043902+RhythmDeolus@users.noreply.github.com>
2024-03-02 17:08:20 +0530

    fixed issues with his alias

--- shell-scripts/alias.sh
+++ shell-scripts/alias.sh
@@ -1,6 +1,6 @@
 export HOME2="/mnt/c/Users/rhyth"
-alias cdd="cd \$(z | fzf | awk '{print \$2}' | grep '^' || find ~ -type d | fzf | grep '^' || pwd)"
-alias his="\$(history | fzf --tiebreak=index --tac | awk  '{sub(/^\\s[0-9]+\\s/, \"\", \$0); print \$0}')"
+alias cdd="cd \$(z | fzf --tiebreak=index --tac | awk '{print \$2}' | grep '^' || find ~ -type d | fzf | grep '^' || pwd)"
+alias his="\$(history | fzf --tiebreak=index --tac | awk  '{sub(/^\\s+[0-9]+\\s/, \"\", \$0); print \$0}')"
 alias xman="cat ~/command_list.txt | fzf | xargs -I{} bash -c 'man {} || {} --help'"
 function vact() {
 source $1"/bin/activate"
