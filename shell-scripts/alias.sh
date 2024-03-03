0a45c69e1902b65ae658afe9aef85dcf8dc40a50
RhythmDeolus <122043902+RhythmDeolus@users.noreply.github.com>
2024-03-03 20:51:21 +0530

    fixed his command

--- shell-scripts/alias.sh
+++ shell-scripts/alias.sh
@@ -1,7 +1,7 @@
 export HOME2="/mnt/c/Users/rhyth"
 alias cdd="cd \$(z | fzf --tiebreak=index --tac | awk '{print \$2}' | grep '^' || find ~ -type d | fzf | grep '^' || pwd)"
-alias his="\$(history | fzf --tiebreak=index --tac | awk  '{sub(/^\\s+[0-9]+\\s/, \"\", \$0); print \$0}')"
-alias xman="cat ~/command_list.txt | fzf | xargs -I{} bash -c 'man {} || {} --help'"
+alias his="eval \"\$(history | fzf --tiebreak=index --tac | awk  '{sub(/^\\s+[0-9]+\\s/, \"\", \$0); print \$0}')\""
+alias xman="cat ~/command_list.txt | fzf | xargs -I{} bash -c 'man {} || {} --help | less'"
 function vact() {
 source $1"/bin/activate"
 }
