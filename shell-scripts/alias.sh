0f9c7d69085e5e3159fd484045729ccdd4d40aba
RhythmDeolus <122043902+RhythmDeolus@users.noreply.github.com>
2024-03-04 22:22:28 +0530

    his now reflects on past history

--- shell-scripts/alias.sh
+++ shell-scripts/alias.sh
@@ -1,6 +1,10 @@
 export HOME2="/mnt/c/Users/rhyth"
 alias cdd="cd \$(z | fzf --tiebreak=index --tac | awk '{print \$2}' | grep '^' || find ~ -type d | fzf | grep '^' || pwd)"
-alias his="eval \"\$(history | fzf --tiebreak=index --tac | awk  '{sub(/^\\s+[0-9]+\\s/, \"\", \$0); print \$0}')\""
+function his() {
+    l=$(history | fzf --tiebreak=index --tac | awk '{$1=""; print $0}')
+    eval "$l"
+    history -s $l
+}
 alias xman="cat ~/command_list.txt | fzf | xargs -I{} bash -c 'man {} || {} --help | less'"
 function vact() {
 source $1"/bin/activate"
